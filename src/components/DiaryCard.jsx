3a3d8f9846d2445d631fd9aa0a209684d7ec1331
Riya Alex <riyaalexx@gmail.com>
2026-06-14 15:29:04 +0200

    added date validations

--- src/components/DiaryCard.jsx
+++ src/components/DiaryCard.jsx
@@ -2,16 +2,16 @@ const DiaryCard = ({ entry, onClick }) => {
   return (
     <div
       onClick={onClick}
-      className="shadow-xl hover:shadow-2xl cursor-pointer rounded-md"
+      className="shadow-xl hover:shadow-2xl cursor-pointer rounded-2xl"
     >
       <img
         src={entry.imgUrl}
         alt={entry.title}
-        className="w-full h-64 object-cover"
+        className="w-full h-65 object-cover rounded-t-2xl"
       />
 
       <div className="p-4">
-        <h2>{entry.title}</h2>
+        <h2 className="font-semibold py-3">{entry.title}</h2>
         <p>{entry.date}</p>
       </div>
     </div>
